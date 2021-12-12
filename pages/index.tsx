cf8a1014bb5ce5fe81471ddad032571d8b481b80
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-12-12 16:44:54 +0000

    config for project

---
+++ pages/index.tsx
@@ -0,0 +1,73 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Character Selector</title>
+        <meta name="description" content="Rick and morty Characters!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Character 1  &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Name and Stats</p>
+          </a>
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.github.com/jordt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by JordT
+          <span className={styles.logo}>
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home

3ed8442ce29d98d5174d0bbda5e2383106b432a0
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-12-18 11:23:52 +0000

    fleshed out character page

--- pages/characterPage/[CharacterPage].tsx
+++ pages/characterPage/[CharacterPage].tsx
@@ -1,7 +1,9 @@
 import { useRouter } from "next/router";
 import Image from 'next/image'
+import Head from 'next/head'
 import { useEffect, useState } from "react";
 import axios from "axios";
+import styles from '../../styles/Home.module.css'
 
 const CharacterPage = () => {
   const router = useRouter()
@@ -19,26 +21,34 @@ const CharacterPage = () => {
   }, [])
   
   return (
-    <div>
-      <a href="http://localhost:3000">Home</a>
-      <p>Name: {characterInfo.name}</p>
-      <p>Status: {characterInfo.status}</p>
-      <p>Species: {characterInfo.species}</p>
-      <p>Gender: {characterInfo.gender}</p>
-      { loading ? <p></p> : <p>Origin: {characterInfo.origin.name}</p>}
-      { loading ? <p></p> : <p>Last know location: {characterInfo.location.name}</p>}
-      { loading ? <p></p> : <p>Number of appearances: {characterInfo.episode.length}</p>}
-      { loading ? <p></p> : <Image src={characterInfo.image} height='300' width='300' />}
+    <div >
+      <Head>
+        <title>Character Page</title>
+        <meta name="description" content="Rick and morty Characters pages" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head> 
+      
+      <main className={styles.main} >
+        <a href="http://localhost:3000" className={styles.anchor}><u>Click here to go back to character selection</u></a>
+        <h1>Character Page</h1>
+        <div className={styles.displayContainer}>
+          <div>
+            <p><b>Name:</b> {characterInfo.name}</p>
+            <p><b>Status:</b> {characterInfo.status}</p>
+            <p><b>Species:</b> {characterInfo.species}</p>
+            <p><b>Gender:</b> {characterInfo.gender}</p>
+            { loading ? <p></p> : <p><b>Origin:</b> {characterInfo.origin.name}</p>}
+            { loading ? <p></p> : <p><b>Last know location:</b> {characterInfo.location.name}</p>}
+            { loading ? <p></p> : <p><b>Number of appearances:</b> {characterInfo.episode.length}</p>}
+          </div>
+          <div>
+            { loading ? <p></p> : <Image src={characterInfo.image} height='300' width='300' className={styles.largeImage}/>}
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
 
 export default CharacterPage
 
-// nextjs guide
-// https://nextjs.org/docs/routing/dynamic-routes
-
-/// neeed to work out how to get URL data from hereeeeee....
-
-
-///
